c55c347d9e00c1b2e62c0013d508151af69893e6
maalos <fasterd91@gmail.com>
2022-05-08 22:12:02 +0200

    Added weather support

--- xsetroot-stats-bash/main.sh
+++ xsetroot-stats-bash/main.sh
@@ -4,18 +4,12 @@ while :
 	do
 	DATE=`date +"%d-%m-%y | %T"`
 	TEMP=(`sensors | grep Core`)
+	WTHR=(`curl https://wttr.in/Warsaw?format=1`)
 	#RAMAMT=(`cat /proc/meminfo | grep MemTotal`)
 	#RAMCUR=(`cat /proc/meminfo | grep MemFree`)
 	#RAMUSG=$(( ${RAMAMT[1]} - ${RAMCUR[1]}))
-  # adjust the line below if your ip starts with 10 instead of 192
 	IP=(`ip a | grep 192`)
 	#eval "xsetroot -name '| ${IP[1]} | ${TEMP[2]} | $(((${RAMAMT[1]}/$RAMUSG))) | ${DATE} |'"
-  
-  # tray version
-  #eval "xsetroot -name '| ${IP[1]} | ${TEMP[2]} | ${DATE} | _____'"
-  
-  # no tray version
-	eval "xsetroot -name '| ${IP[1]} | ${TEMP[2]} | ${DATE} |'"
-  
-	sleep 0.5
+	eval "xsetroot -name '| ${IP[1]} | ${TEMP[2]} | ${WTHR[1]} | ${DATE} |'"
+	sleep 1
 	done
