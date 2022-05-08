805d077452f3c33bbad52b3d4ab18d2fd2e86a02
maalos <fasterd91@gmail.com>
2022-05-08 22:31:03 +0200

    Improvements... I think so...

--- xsetroot-stats-bash/main.sh
+++ xsetroot-stats-bash/main.sh
@@ -4,12 +4,12 @@ while :
 	do
 	DATE=`date +"%d-%m-%y | %T"`
 	TEMP=(`sensors | grep Core`)
-	WTHR=(`curl https://wttr.in/Warsaw?format=1`)
+	WTHR=(`curl http://wttr.in/Warsaw?format=1 &`)
 	#RAMAMT=(`cat /proc/meminfo | grep MemTotal`)
 	#RAMCUR=(`cat /proc/meminfo | grep MemFree`)
 	#RAMUSG=$(( ${RAMAMT[1]} - ${RAMCUR[1]}))
 	IP=(`ip a | grep 192`)
 	#eval "xsetroot -name '| ${IP[1]} | ${TEMP[2]} | $(((${RAMAMT[1]}/$RAMUSG))) | ${DATE} |'"
 	eval "xsetroot -name '| ${IP[1]} | ${TEMP[2]} | ${WTHR[1]} | ${DATE} |'"
-	sleep 1
+	sleep 0.5
 	done
